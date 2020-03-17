22eb6f981d90ef2cab6d31fb63d2bebf32298ed8
viru.ai <virutyagi9@Gmail.com>
2020-03-17 16:47:50 +0000

    Initial commit

---
+++ Arrays and Strings/HelloJavaArr.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class HelloJavaArr{
+  public static void main(String[] args){
+    System.out.println("Testing this Environment if I can make it possible.");
+  }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Here I am going to list all the codes which i am going to do in my learning process.");
+  }
+}
