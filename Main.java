0b479856d19be714376846720040e901b883b516
viru.ai <virutyagi9@Gmail.com>
2020-03-17 22:29:00 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Here I am going to list all the codes which i am going to do in my learning process.");
-  }
-}
