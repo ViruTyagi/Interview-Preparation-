433f738c605c60e4f1a0a4c436fb4a22eda03f10
viru.ai <virutyagi9@Gmail.com>
2020-03-17 16:57:35 +0000

    I have created the Folder for Each type of question and test the environment with the sample program

--- Arrays and Strings/HelloJavaArr.java
+++ Arrays and Strings/HelloJavaArr.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class HelloJavaArr{
   public static void main(String[] args){
     System.out.println("Testing this Environment if I can make it possible.");
+    System.out.println("yes so everything is working great now lets get started to make it work. HopeFull there would be more then 50 questions for each of the topic.");
   }
 }
